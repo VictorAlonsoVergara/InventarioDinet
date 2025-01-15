533f0f41fe90e680343fab9d0ff562912c2b5988
Victor Alonso Vergara <victor.vh2402@gmail.com>
2025-01-15 03:55:01 -0500

    Se llaman a los SP de Crear, Editar y Eliminar

--- InventarioDinet/Controllers/InventoryController.cs
+++ InventarioDinet/Controllers/InventoryController.cs
@@ -115,8 +115,11 @@ namespace InventarioDinet.Controllers
             try
             {
                 DbModelEntities context = new DbModelEntities();
-                context.MOV_INVENTARIO.Add(inventario);
-                context.SaveChanges();
+                context.SP_INSERCION_INVENTARIO(inventario.COD_CIA, inventario.COMPANIA_VENTA_3, inventario.ALMACEN_VENTA,
+                    inventario.TIPO_MOVIMIENTO, inventario.TIPO_DOCUMENTO, inventario.NRO_DOCUMENTO, inventario.COD_ITEM_2,
+                    inventario.PROVEEDOR, inventario.ALMACEN_DESTINO, inventario.CANTIDAD, inventario.DOC_REF_1,
+                    inventario.DOC_REF_2, inventario.DOC_REF_3, inventario.DOC_REF_4, inventario.DOC_REF_5, 
+                    inventario.FECHA_TRANSACCION);
                 return RedirectToAction("Index");
             }
             catch
@@ -129,14 +132,45 @@ namespace InventarioDinet.Controllers
         public ActionResult Edit(string codCia, string companiaVenta3, string almacenVenta, string tipoMovimiento, string tipoDocumento, string nroDocumento, string codItem2)
         {
             DbModelEntities context = new DbModelEntities();
-            return View(context.MOV_INVENTARIO.Where(x =>
-                x.COD_CIA == codCia &&
-                x.COMPANIA_VENTA_3 == companiaVenta3 &&
-                x.ALMACEN_VENTA == almacenVenta &&
-                x.TIPO_MOVIMIENTO == tipoMovimiento &&
-                x.TIPO_DOCUMENTO == tipoDocumento &&
-                x.NRO_DOCUMENTO == nroDocumento &&
-                x.COD_ITEM_2 == codItem2).FirstOrDefault());
+            var inventario = context.SP_CONSULTA_INVENTARIO(
+                    codCia: codCia,
+                    companiaVenta3: companiaVenta3,
+                    almacenVenta: almacenVenta,
+                    tipoMovimiento: tipoMovimiento,
+                    tipoDocumento: tipoDocumento,
+                    nroDocumento: nroDocumento,
+                    codItem2: codItem2,
+                    proveedor: null,
+                    almacenDestino: null,
+                    cantidad: null,
+                    docRef1: null,
+                    docRef2: null,
+                    docRef3: null,
+                    docRef4: null,
+                    docRef5: null,
+                    fechaTransaccion: null
+                )
+                .Select(item => new MOV_INVENTARIO
+                {
+                    COD_CIA = item.COD_CIA,
+                    COMPANIA_VENTA_3 = item.COMPANIA_VENTA_3,
+                    ALMACEN_VENTA = item.ALMACEN_VENTA,
+                    TIPO_MOVIMIENTO = item.TIPO_MOVIMIENTO,
+                    TIPO_DOCUMENTO = item.TIPO_DOCUMENTO,
+                    NRO_DOCUMENTO = item.NRO_DOCUMENTO,
+                    COD_ITEM_2 = item.COD_ITEM_2,
+                    PROVEEDOR = item.PROVEEDOR,
+                    ALMACEN_DESTINO = item.ALMACEN_DESTINO,
+                    CANTIDAD = item.CANTIDAD,
+                    DOC_REF_1 = item.DOC_REF_1,
+                    DOC_REF_2 = item.DOC_REF_2,
+                    DOC_REF_3 = item.DOC_REF_3,
+                    DOC_REF_4 = item.DOC_REF_4,
+                    DOC_REF_5 = item.DOC_REF_5,
+                    FECHA_TRANSACCION = item.FECHA_TRANSACCION
+                })
+                .FirstOrDefault();
+            return View(inventario);
         }
 
         // POST: Inventory/Edit/5
@@ -146,8 +180,11 @@ namespace InventarioDinet.Controllers
             try
             {
                 DbModelEntities context = new DbModelEntities();
-                context.Entry(inventario).State = EntityState.Modified;
-                context.SaveChanges();
+                context.SP_ACTUALIZACION_INVENTARIO(inventario.COD_CIA, inventario.COMPANIA_VENTA_3, inventario.ALMACEN_VENTA,
+                    inventario.TIPO_MOVIMIENTO, inventario.TIPO_DOCUMENTO, inventario.NRO_DOCUMENTO, inventario.COD_ITEM_2,
+                    inventario.PROVEEDOR, inventario.ALMACEN_DESTINO, inventario.CANTIDAD, inventario.DOC_REF_1,
+                    inventario.DOC_REF_2, inventario.DOC_REF_3, inventario.DOC_REF_4, inventario.DOC_REF_5,
+                    inventario.FECHA_TRANSACCION);
                 return RedirectToAction("Index");
             }
             catch
@@ -160,14 +197,45 @@ namespace InventarioDinet.Controllers
         public ActionResult Delete(string codCia, string companiaVenta3, string almacenVenta, string tipoMovimiento, string tipoDocumento, string nroDocumento, string codItem2)
         {
             DbModelEntities context = new DbModelEntities();
-            return View(context.MOV_INVENTARIO.Where(x =>
-                x.COD_CIA == codCia &&
-                x.COMPANIA_VENTA_3 == companiaVenta3 &&
-                x.ALMACEN_VENTA == almacenVenta &&
-                x.TIPO_MOVIMIENTO == tipoMovimiento &&
-                x.TIPO_DOCUMENTO == tipoDocumento &&
-                x.NRO_DOCUMENTO == nroDocumento &&
-                x.COD_ITEM_2 == codItem2).FirstOrDefault());
+            var inventario = context.SP_CONSULTA_INVENTARIO(
+                    codCia: codCia,
+                    companiaVenta3: companiaVenta3,
+                    almacenVenta: almacenVenta,
+                    tipoMovimiento: tipoMovimiento,
+                    tipoDocumento: tipoDocumento,
+                    nroDocumento: nroDocumento,
+                    codItem2: codItem2,
+                    proveedor: null,
+                    almacenDestino: null,
+                    cantidad: null,
+                    docRef1: null,
+                    docRef2: null,
+                    docRef3: null,
+                    docRef4: null,
+                    docRef5: null,
+                    fechaTransaccion: null
+                )
+                .Select(item => new MOV_INVENTARIO
+                {
+                    COD_CIA = item.COD_CIA,
+                    COMPANIA_VENTA_3 = item.COMPANIA_VENTA_3,
+                    ALMACEN_VENTA = item.ALMACEN_VENTA,
+                    TIPO_MOVIMIENTO = item.TIPO_MOVIMIENTO,
+                    TIPO_DOCUMENTO = item.TIPO_DOCUMENTO,
+                    NRO_DOCUMENTO = item.NRO_DOCUMENTO,
+                    COD_ITEM_2 = item.COD_ITEM_2,
+                    PROVEEDOR = item.PROVEEDOR,
+                    ALMACEN_DESTINO = item.ALMACEN_DESTINO,
+                    CANTIDAD = item.CANTIDAD,
+                    DOC_REF_1 = item.DOC_REF_1,
+                    DOC_REF_2 = item.DOC_REF_2,
+                    DOC_REF_3 = item.DOC_REF_3,
+                    DOC_REF_4 = item.DOC_REF_4,
+                    DOC_REF_5 = item.DOC_REF_5,
+                    FECHA_TRANSACCION = item.FECHA_TRANSACCION
+                })
+                .FirstOrDefault();
+            return View(inventario);
         }
 
         // POST: Inventory/Delete/5
@@ -177,16 +245,8 @@ namespace InventarioDinet.Controllers
             try
             {
                 DbModelEntities context = new DbModelEntities();
-                MOV_INVENTARIO inventario = context.MOV_INVENTARIO.Where(x =>
-                                            x.COD_CIA == codCia &&
-                                            x.COMPANIA_VENTA_3 == companiaVenta3 &&
-                                            x.ALMACEN_VENTA == almacenVenta &&
-                                            x.TIPO_MOVIMIENTO == tipoMovimiento &&
-                                            x.TIPO_DOCUMENTO == tipoDocumento &&
-                                            x.NRO_DOCUMENTO == nroDocumento &&
-                                            x.COD_ITEM_2 == codItem2).FirstOrDefault();
-                context.MOV_INVENTARIO.Remove(inventario);
-                context.SaveChanges();
+                context.SP_ELIMINACION_INVENTARIO(codCia, companiaVenta3, almacenVenta, tipoMovimiento, tipoDocumento, 
+                    nroDocumento, codItem2);
                 return RedirectToAction("Index");
             }
             catch
