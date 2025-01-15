0ae778f5fc7b6b4b8fd7ff7c82fa57b106c0270e
Victor Alonso Vergara <victor.vh2402@gmail.com>
2025-01-15 03:18:34 -0500

    Se realizan cambios para la vista de listado

--- InventarioDinet/Controllers/InventoryController.cs
+++ InventarioDinet/Controllers/InventoryController.cs
@@ -14,39 +14,92 @@ namespace InventarioDinet.Controllers
         public ActionResult Index()
         {
             DbModelEntities context = new DbModelEntities();
-            var resultado = context.SP_CONSULTA_INVENTARIO(
-                    codCia: null, 
-                    companiaVenta3: null, 
-                    almacenVenta: null, 
-                    tipoMovimiento: null, 
-                    tipoDocumento: null, 
-                    nroDocumento: null, 
+            //Se llama al Stored Procedure y se castea para mostrarlo como objeto MOV_INVENTARIO
+            var inventarios = context.SP_CONSULTA_INVENTARIO(
+                    codCia: null,
+                    companiaVenta3: null,
+                    almacenVenta: null,
+                    tipoMovimiento: null,
+                    tipoDocumento: null,
+                    nroDocumento: null,
                     codItem2: null,
-                    proveedor: null, 
-                    almacenDestino: null, 
-                    cantidad: null, 
-                    docRef1: null, 
-                    docRef2: null, 
-                    docRef3: null, 
-                    docRef4: null, 
-                    docRef5: null, 
+                    proveedor: null,
+                    almacenDestino: null,
+                    cantidad: null,
+                    docRef1: null,
+                    docRef2: null,
+                    docRef3: null,
+                    docRef4: null,
+                    docRef5: null,
                     fechaTransaccion: null
-            ).ToList();
-            return View(context.MOV_INVENTARIO.ToList());
+                )
+                .Select(item => new MOV_INVENTARIO
+                {
+                    COD_CIA = item.COD_CIA,
+                    COMPANIA_VENTA_3 = item.COMPANIA_VENTA_3,
+                    ALMACEN_VENTA = item.ALMACEN_VENTA,
+                    TIPO_MOVIMIENTO = item.TIPO_MOVIMIENTO,
+                    TIPO_DOCUMENTO = item.TIPO_DOCUMENTO,
+                    NRO_DOCUMENTO = item.NRO_DOCUMENTO,
+                    COD_ITEM_2 = item.COD_ITEM_2,
+                    PROVEEDOR = item.PROVEEDOR,
+                    ALMACEN_DESTINO = item.ALMACEN_DESTINO,
+                    CANTIDAD = item.CANTIDAD,
+                    DOC_REF_1 = item.DOC_REF_1,
+                    DOC_REF_2 = item.DOC_REF_2,
+                    DOC_REF_3 = item.DOC_REF_3,
+                    DOC_REF_4 = item.DOC_REF_4,
+                    DOC_REF_5 = item.DOC_REF_5,
+                    FECHA_TRANSACCION = item.FECHA_TRANSACCION
+                })
+                .ToList();
+            return View(inventarios);
         }
 
         // GET: Inventory/Details/5
         public ActionResult Details(string codCia, string companiaVenta3, string almacenVenta, string tipoMovimiento, string tipoDocumento, string nroDocumento, string codItem2)
         {
             DbModelEntities context = new DbModelEntities();
-            return View(context.MOV_INVENTARIO.Where(x =>
-                x.COD_CIA == codCia &&
-                x.COMPANIA_VENTA_3 == companiaVenta3 &&
-                x.ALMACEN_VENTA == almacenVenta &&
-                x.TIPO_MOVIMIENTO == tipoMovimiento &&
-                x.TIPO_DOCUMENTO == tipoDocumento &&
-                x.NRO_DOCUMENTO == nroDocumento &&
-                x.COD_ITEM_2 == codItem2).FirstOrDefault());
+            //Se llama al Stored Procedure y se castea para mostrarlo como objeto MOV_INVENTARIO
+            var inventario = context.SP_CONSULTA_INVENTARIO(
+                    codCia: codCia,
+                    companiaVenta3: companiaVenta3,
+                    almacenVenta: almacenVenta,
+                    tipoMovimiento: tipoMovimiento,
+                    tipoDocumento: tipoDocumento,
+                    nroDocumento: nroDocumento,
+                    codItem2: codItem2,
+                    proveedor: null,
+                    almacenDestino: null,
+                    cantidad: null,
+                    docRef1: null,
+                    docRef2: null,
+                    docRef3: null,
+                    docRef4: null,
+                    docRef5: null,
+                    fechaTransaccion: null
+                )
+                .Select(item => new MOV_INVENTARIO
+                {
+                    COD_CIA = item.COD_CIA,
+                    COMPANIA_VENTA_3 = item.COMPANIA_VENTA_3,
+                    ALMACEN_VENTA = item.ALMACEN_VENTA,
+                    TIPO_MOVIMIENTO = item.TIPO_MOVIMIENTO,
+                    TIPO_DOCUMENTO = item.TIPO_DOCUMENTO,
+                    NRO_DOCUMENTO = item.NRO_DOCUMENTO,
+                    COD_ITEM_2 = item.COD_ITEM_2,
+                    PROVEEDOR = item.PROVEEDOR,
+                    ALMACEN_DESTINO = item.ALMACEN_DESTINO,
+                    CANTIDAD = item.CANTIDAD,
+                    DOC_REF_1 = item.DOC_REF_1,
+                    DOC_REF_2 = item.DOC_REF_2,
+                    DOC_REF_3 = item.DOC_REF_3,
+                    DOC_REF_4 = item.DOC_REF_4,
+                    DOC_REF_5 = item.DOC_REF_5,
+                    FECHA_TRANSACCION = item.FECHA_TRANSACCION
+                })
+                .FirstOrDefault();
+            return View(inventario);
         }
 
         // GET: Inventory/Create
