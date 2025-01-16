82273185edc637bdde79f0f214270fc1103dfdb5
Victor Alonso Vergara <victor.vh2402@gmail.com>
2025-01-15 21:51:25 -0500

    Se colocan los Primary Key como campos obligatorios

--- InventarioDinet/Models/Inventory.cs
+++ InventarioDinet/Models/Inventory.cs
@@ -9,24 +9,31 @@ namespace InventarioDinet.Models
     public class Inventory
     {
         [Display(Name = "Código de Compañía")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string COD_CIA { get; set; }
 
         [Display(Name = "Compañía de Venta")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string COMPANIA_VENTA_3 { get; set; }
 
         [Display(Name = "Almacén de Venta")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string ALMACEN_VENTA { get; set; }
 
         [Display(Name = "Tipo de Movimiento")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string TIPO_MOVIMIENTO { get; set; }
 
         [Display(Name = "Tipo de Documento")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string TIPO_DOCUMENTO { get; set; }
 
         [Display(Name = "Número de Documento")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string NRO_DOCUMENTO { get; set; }
 
         [Display(Name = "Código de Item")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string COD_ITEM_2 { get; set; }
 
         [Display(Name = "Proveedor")]
